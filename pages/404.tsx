41ee86418281b376cb34cd0836635f9eed31fb3b
DD155 <31744748+DD155@users.noreply.github.com>
2023-03-19 11:45:02 -0400

    finish routing section. end at tutorial 14

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { useRouter, NextRouter } from 'next/router'
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h1> 404 Page </h1>
+        </div>
+    )
+}
+
+export default PageNotFound
--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -1,6 +1,24 @@
+import Link from 'next/link'
+import { useRouter, NextRouter } from 'next/router'
+
 const Homepage = () => {
+    const router:NextRouter = useRouter()
+
+    const handleClick = () => {
+        router.push("/docs/2/3")
+    }
+
+
     return (
-        <h1> Home Page </h1>
+        <div>
+            <h1> Home Page </h1>
+            <Link href='/blog'>
+            Blog
+            </Link>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
+        </div>
     )
 }
 
